2f0b9b17b02decaaf01d4e1e43f388b6edf9bb4c
jkomyno <skiabo97@gmail.com>
2019-07-26 14:53:27 +0200

    Exposed defaultTimeoutHandler

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { useTimeoutDefault as default } from './useTimeoutDefault';
 export { useTimeout } from './useTimeout';
 export { TimeoutHandler } from './TimeoutHandler';
+export { defaultTimeoutHandler } from './defaultTimeoutHandler';
